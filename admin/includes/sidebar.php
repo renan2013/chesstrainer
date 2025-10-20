158eed477b73e7f85c3ca5ec19f31b7dca727c12
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-10-20 11:28:19 -0600

    Refactor: restructura el menú de navegación y aplica restricciones de rol

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -10,17 +10,39 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
-                <li class="nav-item"><a class="nav-link" href="gestionar_categorias.php">Categorías</a></li>
-                <li class="nav-item"><a class="nav-link" href="gestionar_publicaciones.php">Publicaciones</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownEdicion" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Edición
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownEdicion">
+                        <li><a class="dropdown-item" href="gestionar_publicaciones.php">Publicaciones</a></li>
+                        <li><a class="dropdown-item" href="gestionar_categorias.php">Categorías</a></li>
+                        <li><a class="dropdown-item" href="anadir_problema.php">Crear Diagrama</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownInformes" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Informes
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownInformes">
+                        <li><a class="dropdown-item" href="gestionar_reportes.php">Reportes</a></li>
+                        <li><a class="dropdown-item" href="base_conocimiento.php">Base de conocimiento</a></li>
+                        <?php if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], ['administrador', 'instructor'])): ?>
+                        <li><a class="dropdown-item" href="ver_resultados.php">Ver Resultados</a></li>
+                        <li><a class="dropdown-item" href="ranking_general.php">Ranking General</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </li>
                 <?php if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], ['administrador', 'instructor'])): ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownGrupos" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Grupos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownGrupos">
+                        <li><a class="dropdown-item" href="grupos.php">Lista de grupos creados</a></li>
+                    </ul>
+                </li>
                 <li class="nav-item"><a class="nav-link" href="gestionar_usuarios.php">Usuarios</a></li>
-                <?php endif; ?>
-                <li class="nav-item"><a class="nav-link" href="gestionar_reportes.php">Reportes</a></li>
-                <li class="nav-item"><a class="nav-link" href="base_conocimiento.php">Base de Conocimiento</a></li>
-                <?php if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], ['administrador', 'instructor'])): ?>
-                <li class="nav-item"><a class="nav-link" href="grupos.php">Grupos</a></li>
-                <li class="nav-item"><a class="nav-link" href="ver_resultados.php">Ver Resultados</a></li>
-                <li class="nav-item"><a class="nav-link" href="ranking_general.php">Ranking General</a></li>
                 <?php endif; ?>
             </ul>
             <ul class="navbar-nav">
