8ef2d666cde1f9cd8f679f5f799c158628051983
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-10-20 13:13:07 -0600

    Style: elimina el enlace al sitio web del desarrollador del pie de página

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -4,7 +4,7 @@
 
     <footer class="footer mt-auto py-3 bg-light">
         <div class="container text-center">
-            <img href="https://ajedrezpuriscal.com/chess_trainer/img/logo_ct.svg" width="200px">
+            <img src="https://ajedrezpuriscal.com/chess_trainer/img/logo_ct.svg" width="200px">
             <span class="text-muted">developed by renangalvan.net - (+506) 87777849 - San José, Costa Rica. 2025</span>
         </div>
     </footer>
