b1ced841ad1631ead9ecf4752986ada1f4dc0354
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-10-21 14:59:40 -0600

    detalle

--- admin/anadir_problema.php
+++ admin/anadir_problema.php
@@ -182,9 +182,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
 
 
-                <div id="pgn-container" class="col-md-4 mt-3" style="border-radius: 15px; "> <!-- For the PGN moves -->
+                <div id="pgn-container" class="col-md-4 mt-3" > <!-- For the PGN moves -->
                     <p>Movimientos</p>
-                    <div id="pgn-moves" class="border p-3 bg-white" style="height: 360px; overflow-y: auto;">
+                    <div id="pgn-moves" class="border p-3 bg-white" style="height: 360px; overflow-y: auto; border-radius: 15px;">
                         <div class="d-flex justify-content-between">
                             <p class="text-center">Blancas</p>
                             <p class="text-center">Negras</p>
