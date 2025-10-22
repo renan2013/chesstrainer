e55c6e088102e0ec94fc34decbee33e7c20c17ab
JimenaJuarez <jimenahidalgpjuarez888@gmail.com>
2025-10-21 22:59:39 -0600

    d

--- admin/anadir_problema.php
+++ admin/anadir_problema.php
@@ -278,7 +278,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <textarea name="desarrollo" id="desarrollo" class="form-control"></textarea>
                     </div>
 
-                    <div class="col-md-12">
+                    <div class="d-grid">
                         <button type="submit" class="btn btn-primary">Añadir Diagrama - Estudio</button>
                     </div>
 
